150ac3fbe335493bf4d707de54c1e3c13a8cf4b9
Yuvan <yuvankumar2077@gmail.com>
2022-11-26 09:24:24 +0530

    Removed Flask-Bootstrap completely

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask, Blueprint
-from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 
 from config import config
 
 db = SQLAlchemy()
-bootstrap = Bootstrap()
 api = Api()
 
 
@@ -15,7 +13,6 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     db.init_app(app)
-    bootstrap.init_app(app)
 
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
