64337b6eeeb08e05bc0e348f4d69c7f3b3c07f1e
Yuvan <yuvankumar2077@gmail.com>
2022-12-13 22:55:35 +0530

    Fixed a bug
    Fixed a bug which occurs when creating database

--- Flask/manage.py
+++ Flask/manage.py
@@ -1,8 +1,12 @@
+import os
+
 from app import create_app, db
 
 app = create_app('default')
 
 if __name__ == '__main__':
     with app.app_context():
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
         db.create_all()
     app.run(debug=True, host='0.0.0.0', port=80)
